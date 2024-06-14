9a032819318142bbecd59965acaaa0d4b7326414
Erika Silverio <cms.silverio@gmail.com>
2024-06-14 16:30:20 -0300

    Node.js Criando Servidor HTML com ruta absoluta

--- node-02/index.js
+++ node-02/index.js
@@ -1,9 +1,23 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path")
+
+
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type" : "text/plain" });
-    res.end("Hola de HTTP");
+
+            // console.log(__dirname);            
+            // console.log(path.join(__dirname, "public", "index.html"));
+            //     const pathFile = "./public/index.html";
+
+        const pathFile = path.join(__dirname, "public", "index.html");
+
+        const html = fs.readFileSync(pathFile);
+
+    res.writeHead(200, { "Content-Type" : "text/html; charset=utf-8" });
+    res.end(html);
 });
+
 
 const PORT = 3000;
 
